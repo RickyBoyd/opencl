0d25355fd0fd3a7b372b019c5b5471d41b048231
Tom Deakin <tom.deakin@bristol.ac.uk>
2016-09-15 14:09:34 +0100

    Use tt for timestep loop counter

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -154,13 +154,13 @@ int main(int argc, char* argv[])
   gettimeofday(&timstr, NULL);
   tic = timstr.tv_sec + (timstr.tv_usec / 1000000.0);
 
-  for (int ii = 0; ii < params.maxIters; ii++)
+  for (int tt = 0; tt < params.maxIters; tt++)
   {
     timestep(params, cells, tmp_cells, obstacles);
-    av_vels[ii] = av_velocity(params, cells, obstacles);
+    av_vels[tt] = av_velocity(params, cells, obstacles);
 #ifdef DEBUG
-    printf("==timestep: %d==\n", ii);
-    printf("av velocity: %.12E\n", av_vels[ii]);
+    printf("==timestep: %d==\n", tt);
+    printf("av velocity: %.12E\n", av_vels[tt]);
     printf("tot density: %.12E\n", total_density(params, cells));
 #endif
   }
